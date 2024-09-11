16a1e0f229eaee6cb752f0a9fe57ba040a9c9e23
prashasthi1 <prashasthipaboda@gmail.com>
2024-09-11 23:59:39 +0530

    Add Order Item

--- src/main/java/com/example/resturant/controller/OrderItemListController.java
+++ src/main/java/com/example/resturant/controller/OrderItemListController.java
@@ -4,10 +4,83 @@
  */
 package com.example.resturant.controller;
 
+import com.example.resturant.data.OrderItemList;
+import com.example.resturant.service.OrderItemListService;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author ASUS
  */
+@RestController
+@RequestMapping("/api")
 public class OrderItemListController {
-    
+
+    @Autowired
+    OrderItemListService orderItemListController;
+
+    @CrossOrigin
+    @PostMapping(path = "/set-order-item")
+    public OrderItemList setOrderItem(@RequestBody OrderItemList orderItemList) {
+        return orderItemListController.setOrderItem(orderItemList);
+    }
+
+    @CrossOrigin
+    @PutMapping(path = "/update-order-item/{orderItemId}")
+    public OrderItemList updateOrderItemByOrderItemId(
+            @RequestBody OrderItemList orderItemList, 
+            @PathVariable("orderItemId") int orderItemId) {
+        return orderItemListController.updateOrderItemByOrderItemId(orderItemList, orderItemId);
+    }
+
+    @CrossOrigin
+    @GetMapping(path = "/view-all-order-items")
+    public List<OrderItemList> viewAllOrderItem() {
+        return orderItemListController.viewAllOrderItem();
+    }
+
+    @CrossOrigin
+    @GetMapping(path = "/view-all-order-items/{orderId}")
+    public List<OrderItemList> viewAllOrderItemByOrderId(
+            @PathVariable("orderId") int orderId) {
+        return orderItemListController.viewAllOrderItemByOrderId(orderId);
+    }
+
+    @CrossOrigin
+    @GetMapping(path = "/view-order-item/{orderItemId}")
+    public Optional<OrderItemList> viewOrderItemByOrderItemid(
+            @PathVariable("orderItemId") int orderItemId) {
+        return orderItemListController.viewAllOrderItemByOrderItemid(orderItemId);
+    }
+
+    @CrossOrigin
+    @DeleteMapping(path = "/delete-all-order-items")
+    public void deleteOrderItems() {
+        orderItemListController.deleteOrderItems();
+    }
+
+    @CrossOrigin
+    @DeleteMapping(path = "/delete-all-order-items/{orderId}")
+    public void deleteOrderItemsByOrderId(
+            @PathVariable("orderId") int orderId) {
+        orderItemListController.deleteOrderItemsByOrderId(orderId);
+    }
+
+    @CrossOrigin
+    @DeleteMapping(path = "/delete-order-items/{orderItemId}")
+    public void deleteOrderItemByOrderItemid(
+            @PathVariable("orderItemId") int orderItemId) {
+        orderItemListController.deleteOrderItemByOrderItemid(orderItemId);
+    }
 }
--- src/main/java/com/example/resturant/service/OrderItemListService.java
+++ src/main/java/com/example/resturant/service/OrderItemListService.java
@@ -4,10 +4,63 @@
  */
 package com.example.resturant.service;
 
+import com.example.resturant.data.OrderItemList;
+import com.example.resturant.data.OrderItemListRepository;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  *
  * @author ASUS
  */
+@Service
 public class OrderItemListService {
+
+    @Autowired
+    OrderItemListRepository orderItemListRepositiry;
+
+    public OrderItemList setOrderItem(OrderItemList orderItemList) {
+        return orderItemListRepositiry.save(orderItemList);
+    }
+
+    public OrderItemList updateOrderItemByOrderItemId(OrderItemList orderItemList, int orderItemId) {
+        if (orderItemListRepositiry.findById(orderItemId).isPresent()) {
+            orderItemList.setMealId(orderItemList.getMealId());
+            orderItemList.setOrderId(orderItemList.getOrderId());
+            orderItemList.setPrice(orderItemList.getPrice());
+            orderItemList.setQuantity(orderItemList.getQuantity());
+            orderItemList.setOrderItemId(orderItemId);
+            return orderItemListRepositiry.save(orderItemList);
+        } else{
+            return null;
+        }
+    }
     
+    public List<OrderItemList> viewAllOrderItem(){
+      return orderItemListRepositiry.findAll();
+    }
+    
+    public List<OrderItemList> viewAllOrderItemByOrderId(int orderId){
+      return orderItemListRepositiry.findAllByOrderId(orderId);
+    }
+    
+    public Optional<OrderItemList> viewAllOrderItemByOrderItemid(int orderItemId){
+      return orderItemListRepositiry.findById(orderItemId);
+    }
+    
+    public void deleteOrderItems() {
+        orderItemListRepositiry.deleteAll();
+    }
+
+    @Transactional
+    public void deleteOrderItemsByOrderId(int orderId) {
+        orderItemListRepositiry.deleteAllByOrderId(orderId);
+    }
+
+    public void deleteOrderItemByOrderItemid(int orderItemId) {
+        orderItemListRepositiry.deleteById(orderItemId);
+    }
 }
